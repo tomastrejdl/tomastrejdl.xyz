a8c60a6d589e8fe552db435cb24e045f304f1d4e
Tomáš Trejdl <trejdl.tomas@gmail.com>
2022-12-30 17:31:59 +0100

    fix: Remove example router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,11 +1,9 @@
 import { router } from "../trpc";
-import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
+import { authRouter } from './auth'
 
 export const appRouter = router({
-  example: exampleRouter,
   auth: authRouter,
-});
+})
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
