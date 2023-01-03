4b026f0caa5eef359c67f48b156750137b5eaf2e
Tomáš Trejdl <trejdl.tomas@gmail.com>
2023-01-03 23:00:57 +0100

    Updated layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,18 @@
 import { ArrowUpIcon } from '@heroicons/react/24/outline'
-import Link from 'next/link'
 import Button from './Button'
+import { ExternalLink } from './Links'
 
-export default function Footer() {
+export default function Footer({ className }: { className?: string }) {
   const scrollToTop = () => window.scrollTo({ top: 0, behavior: 'smooth' })
   return (
-    <footer className="flex w-full flex-col items-center justify-center gap-4">
-      <div className="flex w-full items-center justify-between">
-        <span>Tomáš Trejdl</span>
+    <footer
+      className={`relative mb-16 mt-8 flex w-full flex-col items-center justify-center gap-4 md:mt-24 md:mb-0  ${className}`}
+    >
+      <div className="pointer-events-none right-0 mb-8 flex w-full items-center justify-end md:absolute md:bottom-0 md:mb-0">
         <Button
           onClick={scrollToTop}
           tooltip="Scroll to top"
-          className="h-10 w-10"
+          className="pointer-events-auto flex h-10 w-10 items-center justify-center rounded-md bg-neutral-200/50 backdrop-blur-sm dark:bg-neutral-800/50"
           aria-label="Scroll to top"
         >
           <span className="sr-only">Scroll to top</span>
@@ -19,16 +20,15 @@ export default function Footer() {
         </Button>
       </div>
 
-      <div className="mx-auto w-full text-center text-xs text-neutral-600 dark:text-neutral-400">
-        <span>© 2023 Tomáš Trejdl | </span>
-        <span>Designed and built in Prague | </span>
-        <Link
+      <div className="flex w-full flex-wrap items-baseline justify-center gap-2 text-xs text-neutral-600 dark:text-neutral-400">
+        <span>© 2023 Tomáš Trejdl</span>•
+        <span>Designed and built in Prague</span>•
+        <ExternalLink
           className="whitespace-nowrap underline hover:text-neutral-900 dark:hover:text-neutral-400"
           href="https://github.com/tomastrejdl/tomastrejdl.xyz"
-          target={'_blank'}
         >
           Source on GitHub
-        </Link>
+        </ExternalLink>
       </div>
     </footer>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import { type NextPage } from 'next'
-import Head from 'next/head'
 import Link from 'next/link'
 import MagicText from '../components/MagicText'
 import SocialLogos from '../components/SocialLogos'
@@ -7,46 +6,35 @@ import HomepageLayout from '../layouts/HomepageLayout'
 
 const HomePage: NextPage = () => {
   return (
-    <>
-      <Head>
-        <title>Tomáš Trejdl - UX Designer and full-stack developer</title>
-        <meta
-          name="description"
-          content="UX Designer and full-stack developer"
-        />
-      </Head>
+    <HomepageLayout>
       <HeroSection />
-    </>
+    </HomepageLayout>
   )
 }
 
 const HeroSection = () => {
   return (
-    <HomepageLayout>
-      <div className="flex grow flex-col items-center justify-center">
-        <h1 className="text-center">
-          <p className="block text-lg lg:text-xl">Hi, my name is Tomáš</p>
-          <p className="mt-8 inline-block text-3xl font-medium sm:text-4xl md:text-5xl lg:text-7xl lg:leading-snug">
-            I&apos;m a <MagicText>UX designer</MagicText> and
-            <br />
-            <MagicText>full-stack developer</MagicText>
-          </p>
-        </h1>
-        <p className="relative mt-4 text-center text-lg">
-          Currently working on{' '}
-          <Link href="https://getwrest.com" className="group">
-            <span>Wrest</span>
-            <span className="absolute -right-3 top-0.5 flex h-2 w-2">
-              <span className="group-hover:magic absolute inline-flex h-full w-full animate-ping rounded-full bg-blue-400 opacity-75"></span>
-              <span className="group-hover:magic relative inline-flex h-2 w-2 rounded-full bg-blue-600 dark:bg-blue-500"></span>
-            </span>
-          </Link>
+    <div className="flex grow flex-col items-center justify-center">
+      <h1 className="text-center">
+        <p className="block text-lg lg:text-xl">Hi, my name is Tomáš</p>
+        <p className="mt-8 inline-block text-3xl font-medium sm:text-4xl md:text-5xl lg:text-7xl lg:leading-snug">
+          I&apos;m a <MagicText>UX designer</MagicText>
         </p>
-        <div className="mt-16">
-          <SocialLogos />
-        </div>
+      </h1>
+      <p className="relative mt-4 text-center text-lg">
+        Currently working on{' '}
+        <Link href="https://getwrest.com" className="group">
+          <span>Wrest</span>
+          <span className="absolute -right-3 top-0.5 flex h-2 w-2">
+            <span className="group-hover:magic absolute inline-flex h-full w-full animate-ping rounded-full bg-blue-400 opacity-75"></span>
+            <span className="group-hover:magic relative inline-flex h-2 w-2 rounded-full bg-blue-600 dark:bg-blue-500"></span>
+          </span>
+        </Link>
+      </p>
+      <div className="mt-16">
+        <SocialLogos />
       </div>
-    </HomepageLayout>
+    </div>
   )
 }
 
