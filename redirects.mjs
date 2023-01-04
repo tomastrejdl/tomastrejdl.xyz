0ab9d6e4bcd9893958cf5a8321c84f4db8a2e018
Tomáš Trejdl <trejdl.tomas@gmail.com>
2023-01-04 09:31:38 +0100

    Add permanent:false to all redirects

--- redirects.mjs
+++ redirects.mjs
@@ -1,43 +1,94 @@
 const redirects = [
-  { source: '/home', destination: '/' },
-  { source: '/about-me', destination: '/about' },
-  { source: '/contact', destination: '/about' },
-  { source: '/work', destination: '/projects' },
-  { source: '/project', destination: '/projects' },
-  { source: '/wrest', destination: '/projects/wrest' },
-  { source: '/smart-triage', destination: '/projects/smart-triage' },
+  {
+    source: '/home',
+    destination: '/',
+    permanent: false,
+  },
+  {
+    source: '/about-me',
+    destination: '/about',
+    permanent: false,
+  },
+  {
+    source: '/contact',
+    destination: '/about',
+    permanent: false,
+  },
+  {
+    source: '/work',
+    destination: '/projects',
+    permanent: false,
+  },
+  {
+    source: '/project',
+    destination: '/projects',
+    permanent: false,
+  },
+  {
+    source: '/wrest',
+    destination: '/projects/wrest',
+    permanent: false,
+  },
+  {
+    source: '/smart-triage',
+    destination: '/projects/smart-triage',
+    permanent: false,
+  },
   {
     source: '/smart-home-platform',
     destination: '/projects/smart-home-platform',
+    permanent: false,
   },
-  { source: '/smart-home', destination: '/projects/smart-home-platform' },
+  {
+    source: '/smart-home',
+    destination: '/projects/smart-home-platform',
+    permanent: false,
+  },
   {
     source: '/instagram',
     destination: 'https://instagram.com/tomastrejdl',
+    permanent: false,
   },
   {
     source: '/linkedin',
     destination: 'https://linkedin.com/in/tomastrejdl',
+    permanent: false,
   },
   {
     source: '/linked-in',
     destination: 'https://linkedin.com/in/tomastrejdl',
+    permanent: false,
   },
-  { source: '/github', destination: 'https://github.com/tomastrejdl' },
-  { source: '/git', destination: 'https://github.com/tomastrejdl' },
+  {
+    source: '/github',
+    destination: 'https://github.com/tomastrejdl',
+    permanent: false,
+  },
+  {
+    source: '/git',
+    destination: 'https://github.com/tomastrejdl',
+    permanent: false,
+  },
   {
     source: '/dribbble',
     destination: 'https://dribbble.com/tomastrejdl',
+    permanent: false,
   },
   {
     source: '/dribble',
     destination: 'https://dribbble.com/tomastrejdl',
+    permanent: false,
   },
   {
     source: '/drible',
     destination: 'https://dribbble.com/tomastrejdl',
+    permanent: false,
   },
-  { source: '/resume', destination: 'https://rsm.io/tomastrejdl' },
+  {
+    source: '/resume',
+    destination: 'https://rsm.io/tomastrejdl',
+    permanent: false,
+  },
 ]
 
 export default redirects
