672d3998ccc8c371736887a2818bca771d11bc43
Tomáš Trejdl <trejdl.tomas@gmail.com>
2023-01-04 14:52:34 +0100

    Refactor Homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,88 +1,130 @@
 import { type InferGetStaticPropsType } from 'next'
-import Image from 'next/image'
-import { InternalLink } from '../components/Links'
 import ProjectCard from '../components/ProjectCard'
-// import Link from 'next/link'
-// import MagicText from '../components/MagicText'
-import SocialLogos from '../components/SocialLogos'
-import BaseLayout from '../layouts/BaseLayout'
-import { getAllPublished } from '../server/common/notion'
+import {
+  getAllPublished,
+  type NotionCMSItemMedatada,
+} from '../server/common/notion'
+import { Transition } from '@headlessui/react'
+import HomepageLayout from '../layouts/HomepageLayout'
+import { CustomLink } from '../components/CustomLink'
+import MagicText from '../components/MagicText'
 
 export default function HomePage({
-  wrestProject,
+  projects,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <BaseLayout>
-      <div className="my-16 flex flex-wrap items-center gap-8 md:my-4 xl:my-16">
-        <Image
-          src="/img/tomastrejdl-headshot-no-bg.png"
-          alt="Tomáš Trejdl photo"
-          width={3024 / 30}
-          height={4032 / 30}
-          className="my-0 aspect-square h-20 w-20 flex-shrink-0 overflow-hidden rounded-full border bg-neutral-100 object-contain dark:border-neutral-700 dark:bg-neutral-800 sm:h-28 sm:w-28"
-          priority={true}
-        />
-        <div>
-          <h1 className="mb-0 text-2xl sm:text-4xl">Tomáš Trejdl</h1>
-          <p className="my-1 text-base font-semibold sm:text-lg">
-            Student, UX Designer
-          </p>
-        </div>
-
-        <SocialLogos className="md:ml-auto" />
-      </div>
-
-      <section className="mt-10 sm:mt-12 md:mt-16 lg:mt-20">
-        <header className="mb-4 flex items-center justify-between">
-          <h2 className="text-lg font-medium text-neutral-600 dark:text-neutral-400">
-            My latest project
-          </h2>
-          <InternalLink
-            href="/projects"
-            className="text-blue-500 dark:text-blue-400"
-          >
-            See all
-          </InternalLink>
-        </header>
-        {wrestProject && <ProjectCard item={wrestProject} priority />}
-      </section>
-      {/* <HeroSection /> */}
-    </BaseLayout>
+    <HomepageLayout>
+      <HeroSection />
+      <ProjectsSection projects={projects} />
+    </HomepageLayout>
   )
 }
 
-// const HeroSection = () => {
-//   return (
-//     <div className="flex grow flex-col items-center justify-center">
-//       <h1 className="text-center">
-//         <p className="block text-lg lg:text-xl">Hi, my name is Tomáš</p>
-//         <p className="mt-8 inline-block text-3xl font-medium sm:text-4xl md:text-5xl lg:text-7xl lg:leading-snug">
-//           I&apos;m a <MagicText>UX designer</MagicText>
-//         </p>
-//       </h1>
-//       <p className="relative mt-4 text-center text-lg">
-//         Currently working on{' '}
-//         <Link href="https://getwrest.com" className="group">
-//           <span>Wrest</span>
-//           <span className="absolute -right-3 top-0.5 flex h-2 w-2">
-//             <span className="group-hover:magic absolute inline-flex h-full w-full animate-ping rounded-full bg-blue-400 opacity-75"></span>
-//             <span className="group-hover:magic relative inline-flex h-2 w-2 rounded-full bg-blue-600 dark:bg-blue-500"></span>
-//           </span>
-//         </Link>
-//       </p>
-//       <div className="mt-16">
-//         <SocialLogos />
-//       </div>
-//     </div>
-//   )
-// }
+const HeroSection = () => (
+  <section className="flex min-h-[60dvh] grow flex-col items-center justify-center">
+    <h1 className="text-center">
+      <p className="block text-lg lg:text-xl">Hi, my name is Tomáš</p>
+      <p className="mt-8 inline-block text-3xl font-medium sm:text-4xl md:text-5xl lg:text-7xl lg:leading-snug">
+        I&apos;m a <MagicText>student</MagicText> <br />
+        <MagicText>UX designer</MagicText>
+      </p>
+    </h1>
+    <p className="relative mt-4 text-center text-lg">
+      Currently working on{' '}
+      <CustomLink
+        href="/projects/wrest"
+        className="hover:magic-text group inline-block font-medium text-blue-600 transition will-change-transform dark:text-blue-500"
+      >
+        <span>Wrest</span>
+        <span className="absolute -right-3 top-0.5 flex h-2 w-2">
+          <span className="group-hover:magic absolute inline-flex h-full w-full animate-ping rounded-full bg-blue-400 opacity-75"></span>
+          <span className="group-hover:magic relative inline-flex h-2 w-2 rounded-full bg-blue-600 dark:bg-blue-500"></span>
+        </span>
+      </CustomLink>
+    </p>
+  </section>
+)
 
+// const HeroSection = () => (
+//   <section className="flex min-h-[60dvh] grow flex-col items-center justify-center">
+//     <h1 className="sr-only">
+//       Hello world, my name is Tomáš. I'm a UX designer and student
+//     </h1>
+//     <h1 aria-hidden className="h-32 w-full">
+//       <TypeAnimation
+//         sequence={['Hello world, my name is Tomáš']}
+//         wrapper="span"
+//         speed={75}
+//         cursor={false}
+//         className="block text-lg lg:text-xl"
+//       />
+//       <TypeAnimation
+//         sequence={[1000, "I'm a student and UX designer"]}
+//         wrapper="span"
+//         speed={30}
+//         deletionSpeed={50}
+//         cursor={false}
+//         repeat={Infinity}
+//         className="mt-8 inline-block text-3xl font-medium sm:text-4xl md:text-5xl lg:text-5xl lg:leading-snug"
+//       />
+//     </h1>
+//     <TypeAnimation
+//       sequence={[2000, 'Currently working on ']}
+//       wrapper="p"
+//       speed={30}
+//       deletionSpeed={50}
+//       cursor={false}
+//       repeat={Infinity}
+//       className="relative mt-16 h-5 w-full text-lg"
+//     />
+//     {/* <p className="relative mt-4 text-center text-lg">
+//       Currently working on{' '}
+//       <CustomLink
+//         href="/projects/wrest"
+//         className="hover:magic-text group inline-block font-medium text-blue-600 transition will-change-transform dark:text-blue-500"
+//       >
+//         <span>Wrest</span>
+//         <span className="absolute -right-3 top-0.5 flex h-2 w-2">
+//           <span className="group-hover:magic absolute inline-flex h-full w-full animate-ping rounded-full bg-blue-400 opacity-75"></span>
+//           <span className="group-hover:magic relative inline-flex h-2 w-2 rounded-full bg-blue-600 dark:bg-blue-500"></span>
+//         </span>
+//       </CustomLink>
+//     </p> */}
+//   </section>
+// )
+
+const ProjectsSection = ({
+  projects,
+}: {
+  projects: NotionCMSItemMedatada[]
+}) => (
+  <Transition
+    show={true}
+    appear
+    enter="duration-500 delay-[1s] ease-out transition"
+    enterFrom="opacity-0 translate-y-64"
+    enterTo="opacity-100 translate-y-0"
+  >
+    <section className="mt-10 sm:mt-12 md:mt-16 lg:mt-20">
+      {/* <header className="mx-auto mb-4 flex w-full items-center justify-between">
+      <h2 className="uppercase text-neutral-500 dark:text-neutral-400">
+        My latest projects
+      </h2>
+    </header> */}
+      <ul className="mx-auto flex flex-col gap-24">
+        {projects.map((item, index) => (
+          <li key={item.slug}>
+            <ProjectCard item={item} priority={index <= 2} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  </Transition>
+)
 export async function getStaticProps() {
   return {
     props: {
-      wrestProject: (await getAllPublished('projects')).find(
-        (project) => project.slug === 'wrest'
-      ),
+      projects: await getAllPublished('projects'),
     },
   }
 }
