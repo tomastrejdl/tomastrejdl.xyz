0a6ba11057ca9340821b12365a79f1fbad18d7f9
Tomáš Trejdl <trejdl.tomas@gmail.com>
2023-01-04 11:15:38 +0100

    Fix type in next-config

--- next.config.mjs
+++ next.config.mjs
@@ -57,7 +57,7 @@ export default withBundleAnalyzer(withPWA(config))
 const ContentSecurityPolicy = `
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline' https://static.highlight.io;
-    worder-src 'self';
+    worker-src 'self';
     child-src;
     style-src 'self' 'unsafe-inline';
     img-src * blob: data:;
